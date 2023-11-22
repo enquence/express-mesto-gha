c804538f4d4acc19ce6211626244aabe6a5cf5f1
Andrey Ognev <enquence@yandex.ru>
2023-11-22 14:48:24 +0300

    fix: better error messages via Schemas and validator

--- models/card.js
+++ models/card.js
@@ -1,15 +1,21 @@
 const mongoose = require('mongoose');
+// eslint-disable-next-line import/no-extraneous-dependencies
+const validator = require('validator');
 
 const CardSchema = new mongoose.Schema({
   name: {
     type: String,
-    required: true,
-    minLength: 2,
-    maxLength: 30,
+    required: [true, 'Поле "name" должно быть заполнено'],
+    minLength: [2, 'Минимальная длина поля "name" - 2'],
+    maxLength: [30, 'Максимальная длина поля "name" - 30'],
   },
   link: {
     type: String,
-    required: true,
+    validate: {
+      validator: (v) => validator.isURL(v),
+      message: 'Некорректный URL',
+    },
+    required: [true, 'Поле "link" должно быть заполнено'],
   },
   owner: {
     type: mongoose.Schema.Types.ObjectId,
